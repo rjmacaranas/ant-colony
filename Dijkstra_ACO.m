24affe1dd90639ec752894c189e399fd0dc6d01c
rjmacaranas <rjmac22899@gmail.com>
2020-05-09 21:51:58 -0600

    Update Dijkstra_ACO

--- Dijkstra_ACO.m
+++ Dijkstra_ACO.m
@@ -13,3 +13,23 @@
 load exampleMaps.mat                    % import example maps
 map = binaryOccupancyMap(simpleMap, 2); % construct map
 
+%------------------------------------------------------------------
+% The following is a test using Probabilistic Roadmap (PRM)
+% to become familiar with path planning in the Mobile Robotic 
+% Algorithms section of the MATLAB Robotics System Toolbox
+% ** Will remove for the final submission
+%------------------------------------------------------------------
+
+robotRadius = 0.2;
+mapInflated = copy(map);
+inflate(mapInflated, robotRadius);
+
+prm = mobileRobotPRM;
+prm.Map = mapInflated;
+prm.NumNodes = 50;
+
+startLocation = [2 1];
+endLocation = [12 2];
+
+path = findpath(prm, startLocation, endLocation);
+show(prm);
