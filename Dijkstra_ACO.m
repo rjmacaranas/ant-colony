1ba862342fce81e2ab367e9dcf67ccd181386cff
rjmacaranas <rjmac22899@gmail.com>
2020-05-12 22:01:19 -0600

    Update A* search

--- Dijkstra_ACO.m
+++ Dijkstra_ACO.m
@@ -27,6 +27,12 @@ endLocation = [12 2];
 xPos = startLocation(2);                % initialize robot x position
 yPos = startLocation(1);                % initialize robot y position
 
+%------------------------------------------------------------------
+% The following is adapted from alexranaldi's A_STAR repository
+% on GitHub which can be found at:
+% https://github.com/alexranaldi/A_STAR
+%------------------------------------------------------------------
+
 path = [];                              % initialize path
 mapDimensions = size(map);              % get dimensions
 mapSize = numel(size(map));             % get # elements
@@ -38,17 +44,91 @@ closedSet = false(mapDimension);
 
 previous = zeros(1, mapSize);
 
-gScore = inf(mapDimensions);            % gScore[n]: cost of cheapest
+gScore = inf(mapDimensions);            % gScore[n]: cost of cheapest move
 gScore(xPos, yPos) = 0;
 
 [gr, gc] = ind2sub(madDimensions, endLocation);
 
 fScore = inf(mapDimensions);
-fScore(xPos, yPos); % need to insert cost function here
+fScore(xPos, yPos)  % need to insert cost function here
 
-% while any(openSet(:) > 0)               % while openSet is not empty
-%     current = min(fScore);
+while any(openSet(:) > 0)
+    
+    [~, current] = min(fScore(:));
+    
+    if current == goal
+        final = get_path(previous, current);
+        return
+    end
+    
+    row = mod(current - 1, mapDimensions(1)) + 1;
+    col = (current - row) / mapDimensions(1) + 1;
+    
+    openSet(row, col) = false;
+    closedSet(row, col) = true;
+    
+    fScore(row, col) = inf;
+    gScoreCurrent = gScore(row, col) + costs(row, col);
+    
+    S2 = sqrt(2);
+    
+    n_ss = [ ...
+            rc + 1, cc + 1, S2 ; ...
+            rc + 1, cc + 0, 1 ; ...
+            rc + 1, cc - 1, S2 ; ...
+            rc + 0, cc - 1, 1 ; ...
+            rc - 1, cc - 1, S2 ; ...
+            rc - 1, cc - 0, 1 ; ... 
+            rc - 1, cc + 1, S2 ; ...
+            rc - 0, cc + 1, 1 ; ...
+    ];
 
+    valid_row = n_ss(:,1) >= 1 & n_ss(:,1) <= mapDimensions(1);
+    valid_col = n_ss(:,2) >= 1 & n_ss(:,2) <= mapDimensions(2);
+    n_ss = n_ss(valid_row & valid_col, :);
+    
+    neighbors = n_ss(:,1) + (n_ss(:,2) - 1) .* mapDimensions(1);
+    ixInMap = map(neighbors) & ~closedSet(neighbors);
+    neighbors = neighbors(ixInMap);
+    
+    dists = n_ss(ixInMap, 3);
+    
+    openSet(neighbors) = true;
+    
+    tentative_gscores = gScoreCurrent + costs(neighbors) .* dists;
+    
+    ixBetter = tentative_gscores < gScore(neighbors);
+    bestNeighbors = neighbors(ixBetter);
+    
+    previous(bestNeighbors) = current;
+    gScore(bestNeighbors) = tentative_gscores(ixBetter);
+    fScore(bestNeighbors) = gScore(bestNeighbors) + ...
+                    compute_cost(mapDimensions, bestNeighbors, gr, gc);
+    
+end
+
+function p = get_path(previous, current)
+    % Returns the path. This function is only called once and therefore
+    %   does not need to be extraordinarily efficient
+    inds = find(previous);
+    p = nan(1, length(inds));
+    p(1) = current;
+    next = 1;
+    while any(current == inds)
+        current = previous(current);
+        next = next + 1;
+        p(next) = current; 
+    end
+    p(isnan(p)) = [];
+end
+
+
+function cost = compute_cost(mapSize, from, rTo, cTo)
+    % Returns COST, an estimated cost to travel the map, starting FROM and
+    %   ending at TO.
+    [rFrom,cFrom] = ind2sub(mapSize, from);
+    cost = sqrt((rFrom - rTo).^2 + (cFrom - cTo).^2);
+end
 
 % function path = reconstruct_path(prev, current)
 %     total_path = [current]
--- README.md
+++ README.md
@@ -10,6 +10,10 @@ Cal Poly, SLO  <br> Dr. Helen Yu, Spring 2020
 
 
 ## Updates <a name="updates"></a>
+- May 12, 2020: Continued implementation of A-star search, but could not finish
+applying the algorithm to the simpleMap object in exampleMaps.mat. The task of
+completing this is again pushed to tomorrow. Admittedly did not have much time
+to work today, but tomorrow should be better.
 - May 11, 2020: Began implementation of A-star search path finding in order
 to better understand the MATLAB environment. Hoping to complete this by
 tomorrow in order to duplicate findings in "Research on Robot Path Planning
